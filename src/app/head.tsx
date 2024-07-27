6c84e61802ffdc22463cbc1e3e4b381cda4d82e7
Jorge Edo. Hernández <jorge@galpha.co>
2024-07-27 18:56:02 -0300

    Adjusment fonts

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,10 @@ export default function Head() {
   return (
     <>
       <title>GoLeads</title>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Open+Sans:wght@300;400;600;700&display=swap"
+      />
     </>
   )
 }
